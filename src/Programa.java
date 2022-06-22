87adaea1c18747432377894c4d4c87016cbf129b
saulgiedr1 <saulgiedr@gmail.com>
2022-06-22 16:53:12 +0300

    "Baigta"

--- src/Programa.java
+++ src/Programa.java
@@ -5,12 +5,18 @@ public class Programa {
         double antraSiena = 18;
         double treciaSiena = 20.47;
         double ketvirtaSiena = 16.37;
+        double pirmaSiena1 = 2;
+        double antraSiena1 = 4;
+        double treciaSiena1 = 2;
+        double ketvirtaSiena1 = 4.1;
 
 
         double perimetras = namoPerimetras(pirmaSiena, antraSiena, treciaSiena, ketvirtaSiena);
+        double perimetras1 = namoPerimetras(pirmaSiena1, antraSiena1, treciaSiena1, ketvirtaSiena1);
 
 
         System.out.println("perimetras = " + perimetras);
+        System.out.println("perimetras1 = " + perimetras1);
     }
 
     public static double namoPerimetras(double a, double b, double c, double d){
